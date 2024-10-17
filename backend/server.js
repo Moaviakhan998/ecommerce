67c948078bc379154974376b01de68d727c2dfff
Muhammad Rouifa <rouifa.sanaullah@pitb.gov.pk>
2024-10-17 12:13:39 +0500

    server changes

--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,43 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './db.js';
-import authroute from './Routes/Authroute.js'
-import CategoryRoute from'./Routes/CategoryRoute.js';
-import ProductRoute from './Routes/ProdctRoute.js'
-import cors from "cors"
-//config
- dotenv.config()
+import authroute from './Routes/Authroute.js';
+import CategoryRoute from './Routes/CategoryRoute.js';
+import ProductRoute from './Routes/ProdctRoute.js';
+import cors from 'cors';
 
- //database config
+// config
+dotenv.config();
 
- connectDB();
- //res object
+// database config
+connectDB();
+
+// res object
 const app = express();
-//middelwares
+
+// middlewares
 app.use(cors());
-app.use(express.json())
-app.use(morgan('dev'))
-//routes
-app.use('/api/v1/auth', authroute);
-app.use('/api/v1/category', CategoryRoute)
-app.use('/api/v1/product', ProductRoute)
-//rest api
-app.get('/', (req, res)=>{
-    res.send("<h1>Well come to E-commerce app</h1>");
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Timeout middleware (10 seconds)
+app.use((req, res, next) => {
+    req.setTimeout(10000); // 10 seconds
+    next();
 });
 
-//port
+// routes
+app.use('/api/v1/auth', authroute);
+app.use('/api/v1/category', CategoryRoute);
+app.use('/api/v1/product', ProductRoute);
+
+// rest api
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to E-commerce app</h1>");
+});
+
+// port
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=>{
-    console.log(`Server Running on the ${PORT}` )
-})
+app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
+});
